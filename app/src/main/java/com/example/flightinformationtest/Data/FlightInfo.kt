9be4bf0133c875247faa228ab227a6efec95000e
z09585910work <z09585910work@gmail.com>
2025-04-03 13:05:29 +0800

    2025/04/03進行中

---
+++ app/src/main/java/com/example/flightinformationtest/Data/FlightInfo.kt
@@ -0,0 +1,9 @@
+package com.example.flightinformationtest.Data
+
+data class FlightInfo(
+    val FlightNumber: String?,
+    val AirlineName: String?,
+    val ScheduledTime: String?,
+    val ActualTime: String?,
+    val Status: String?
+)
--- app/src/main/java/com/example/flightinformationtest/Model/CurrencyItem.kt
+++
@@ -1,4 +0,0 @@
-package com.example.flightinformationtest.Model
-
-class CurrencyItem {
-}
