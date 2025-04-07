911c06faccedbdaada307072dd1780cd16a3d5c3
z09585910work <z09585910work@gmail.com>
2025-04-07 16:23:16 +0800

    no message

---
+++ app/src/main/java/com/example/flightinformationtest/Data/CurrencyInfo.kt
@@ -0,0 +1,9 @@
+package com.example.flightinformationtest.Data
+
+data class CurrencyInfo(
+    val JPY: Double,
+    val USD: Double,
+    val CNY: Double,
+    val EUR: Double,
+    val AUD: Double,
+    val KRW: Double)
