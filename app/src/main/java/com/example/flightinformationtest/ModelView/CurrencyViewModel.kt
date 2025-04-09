0b821fd2c591626e4f7747f3723f3ebdae90609e
z09585910work <z09585910work@gmail.com>
2025-04-09 22:39:30 +0800

    新增輸入和轉換匯率參數

--- app/src/main/java/com/example/flightinformationtest/ModelView/CurrencyViewModel.kt
+++ app/src/main/java/com/example/flightinformationtest/ModelView/CurrencyViewModel.kt
@@ -2,6 +2,7 @@ package com.example.flightinformationtest.ModelView
 
 import android.util.Log
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.flightinformationtest.Model.CurrencyRepository
@@ -10,38 +11,58 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
 
-class CurrencyViewModel: ViewModel() {
+class CurrencyViewModel : ViewModel() {
 
     private val repository = CurrencyRepository()
-    val rates:LiveData<Map<String,Double>> get()=repository.rates
-    var updateJobC: Job? =null
+    val rates: LiveData<Map<String, Double>> get() = repository.rates
+    var updateJobC: Job? = null
 
     //private val API_KEY="fca_live_1dJaKxoSAJGMkXq9YgvFP5VLJc6bkJR0QeidcoM2"
-    private val API_KEY="fca_live_tjVSCZqtDkrZzVtbrDHhwcQ6owO3kLxHJ2hXAHpR"
+    private val API_KEY = "fca_live_tjVSCZqtDkrZzVtbrDHhwcQ6owO3kLxHJ2hXAHpR"
 
+    private var _inputAmount = MutableLiveData<Double>()
+    private val inputAmount: LiveData<Double> get() = _inputAmount
 
-    fun loadRates(apiKey:String){
+    private val _convertedRates = MutableLiveData<Map<String, Double>>()
+    private val convertedRates: LiveData<Map<String, Double>> get() = _convertedRates
+
+    fun loadRates(apiKey: String) {
         repository.fetchLatesRates(apiKey)
     }
 
-    fun loadRate(){
-        repository.fetchLatesRates(API_KEY)
-        Log.d("CurrencyViewModel", "loadRate: "+rates)
+    fun setInputAmount(amount: Double) {
+
+        _inputAmount.value = amount
+        CoverRates()
     }
 
-    fun startAutoUpdateC(secons:  Long=10000L){
+    private fun CoverRates() {
+        val baseAmount = _inputAmount.value ?: return
+        val currentRates = rates.value ?: return
 
-        if(updateJobC?.isActive==true) return
+        val converted = currentRates.mapValues { it.value * baseAmount }
+        _convertedRates.value=converted
+    }
 
-        updateJobC=viewModelScope.launch {
 
-            while (isActive){
+    fun loadRate() {
+        repository.fetchLatesRates(API_KEY)
+        Log.d("CurrencyViewModel", "loadRate: " + rates)
+    }
+
+    fun startAutoUpdateC(secons: Long = 10000L) {
+
+        if (updateJobC?.isActive == true) return
+
+        updateJobC = viewModelScope.launch {
+
+            while (isActive) {
 
                 try {
                     loadRate()
                     delay(secons)
                     Log.d("CurrencyViewModel", "OKOKOK ")
-                }catch (e:Exception){
+                } catch (e: Exception) {
 
                     Log.e("CurrencyViewModel", "Fetch error: ${e.message}")
                 }
@@ -49,7 +70,7 @@ class CurrencyViewModel: ViewModel() {
         }
     }
 
-    fun stopFetchingC(){
+    fun stopFetchingC() {
         updateJobC?.cancel()
     }
 
