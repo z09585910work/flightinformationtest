829b94fa632839303d90d9c08798b0bd2af3c84d
z09585910work <z09585910work@gmail.com>
2025-04-09 19:13:00 +0800

    test1

---
+++ app/src/main/java/com/example/flightinformationtest/ModelView/CalculatorViewModel.kt
@@ -0,0 +1,48 @@
+package com.example.flightinformationtest.ModelView
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import net.objecthunter.exp4j.ExpressionBuilder
+
+
+class CalculatorViewModel : ViewModel() {
+
+    private val _output = MutableLiveData<String>()
+    val output: LiveData<String> = _output
+
+    private val inputBuilder = StringBuilder()
+
+    fun onButtonClick(value: String) {
+        inputBuilder.append(value)
+        _output.value = inputBuilder.toString()
+    }
+
+    fun clearInput() {
+        inputBuilder.clear()
+        _output.value = ""
+    }
+
+    fun deleteLastChar() {
+        if (inputBuilder.isNotEmpty()) {
+            inputBuilder.deleteAt(inputBuilder.length - 1)
+            _output.value = inputBuilder.toString()
+        }
+    }
+
+    fun calculateResult() {
+        try {
+            val result = eval(inputBuilder.toString())
+            _output.value = result.toString()
+            inputBuilder.clear()
+            inputBuilder.append(result.toString())
+        } catch (e: Exception) {
+            _output.value = "Error"
+        }
+    }
+
+    private fun eval(expr: String): Double {
+        val expression = ExpressionBuilder(expr).build()
+        return expression.evaluate()
+    }
+}
